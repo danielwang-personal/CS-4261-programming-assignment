f3ba918d075d07c6639cce1799f9efa4c08f3831
danielwang-personal <daniel867181@gmail.com>
2022-09-07 12:57:07 -0400

    initial commit

---
+++ CS4261 programming assignment/CS4261_programming_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS4261_programming_assignmentApp.swift
+//  CS4261 programming assignment
+//
+//  Created by Daniel Wang on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CS4261_programming_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
